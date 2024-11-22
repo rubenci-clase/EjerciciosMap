2ca0ddc65dcfaba92fccff27c9d207ae9309eb7d
rubenci-clase <ruben.corisi@educa.jcyl.es>
2024-11-22 12:53:49 +0100

    Ejercicio3 finalizado

---
+++ src/ejercicio4/Ejer1.java
@@ -0,0 +1,16 @@
+package ejercicio4;
+
+public class Ejer1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] cargos = {"Presidente", "Gobernador"};
+		
+		String[] registros = {"Felipe , 20", 
+				"Juan, 20",
+				"Presidente , Shiva",};
+		
+		
+	}
+
+}
--- src/ejercicio4/Ejer3.java
+++ src/ejercicio4/Ejer3.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 
 public class Ejer3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        String[] registros = {"LOGIN","DOWNLOAD","LOGOUT","LOGIN","LOGIN", "Diego", "Pablo", "asdfgh"};
+        String[] registros = {"LOGIN","DOWNLOAD","LOGOUT","LOGIN","LOGIN", "Diego", "Pablo", "asdfgh", "DiegoDiegoDiegoDiego"};
 
             // Mapa para almacenar las acciones por IP
             HashMap<Integer, List<String>> hashMapPalabras = new HashMap<>();
@@ -24,14 +23,13 @@ public class Ejer3 {
             	if(tamaño < 15 ) {
             		
             		hashMapPalabras.putIfAbsent(tamaño, new ArrayList<>());	
-            	}
-        		
-        		if(hashMapPalabras.get(tamaño).isEmpty() || !palabrasMetidas.contains(registro.toLowerCase())) {
+            		
+            		if(hashMapPalabras.get(tamaño).isEmpty() || !palabrasMetidas.contains(registro.toLowerCase())) {
 
-                	hashMapPalabras.get(tamaño).add(registro.toLowerCase());  
-                	palabrasMetidas.add(registro.toLowerCase());
-                	
-        		}
+                    	hashMapPalabras.get(tamaño).add(registro.toLowerCase());  
+                    	palabrasMetidas.add(registro.toLowerCase());
+            		}
+            	}	
             }
             
             for(Entry<Integer, List<String>> entry : hashMapPalabras.entrySet()) {
@@ -40,7 +38,6 @@ public class Ejer3 {
             }
             
             System.out.println("Lista de palabras: " + hashMapPalabras);
-
 	}
 
 }
