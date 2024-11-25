a073d8b22861034b6d11e4a0e791c8ef866987f0
rubenci-clase <ruben.corisi@educa.jcyl.es>
2024-11-25 11:26:20 +0100

    Ejercicio finalizado

--- src/ejercicio4/Ejer1.java
+++ src/ejercicio4/Ejer1.java
@@ -1,16 +1,40 @@
 package ejercicio4;
 
+import java.util.HashMap;
+
 public class Ejer1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] cargos = {"Presidente", "Gobernador"};
+        HashMap<String, HashMap<String, Integer>> hashMapVotos = new HashMap<>();
 		
-		String[] registros = {"Felipe , 20", 
-				"Juan, 20",
-				"Presidente , Shiva",};
+		String[] registros = {"Presidente , Felipe", "Presidente , Juan","Gobernador , Shiva", "Gobernador , Pablo", "Gobernador , Shiva"};
 		
-		
-	}
+        for (String registro : registros) {
+        	
+            String[] partes = registro.split(" , ");
+        	String cargo = partes[0];
+        	String nombre = partes[1];
+        	HashMap <String, Integer> mapaVoto = new HashMap<>();
+        	mapaVoto.putIfAbsent(nombre, 1);
+        	
+        	if (!hashMapVotos.containsKey(cargo)) { //Si el cargo no existe
+        		hashMapVotos.putIfAbsent(cargo, mapaVoto); //Lo añadimos y añadimos el voto
 
+        	}
+        	else if (hashMapVotos.get(cargo).containsKey(nombre)){ //Si, si existe el cargo y el nombre
+        		HashMap<String, Integer> hashMapInterno = hashMapVotos.get(cargo);
+        		Integer numero = hashMapInterno.get(nombre) + 1;
+        		hashMapInterno.put(nombre, numero);
+        		hashMapVotos.put(cargo, hashMapInterno);
+        	}
+        	else { // Si existe el cargo pero no el nombre
+        		hashMapVotos.get(cargo).putAll(mapaVoto);
+        	}
+        }        
+        
+        for (String innerKey : hashMapVotos.keySet()) {
+            System.out.println("  Cargo: " + innerKey + ", Votos: " + hashMapVotos.get(innerKey));
+        }
+	}
 }
--- src/ejercicio4/Ejer2.java
+++ src/ejercicio4/Ejer2.java
@@ -1,10 +1,40 @@
 package ejercicio4;
 
+import java.util.HashMap;
+
 public class Ejer2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] registros = {"Felipe", "Juan","Shiva", "Pablo"};
+		HashMap<String, HashMap<Character, Integer>>  hashMapPalabras = new HashMap<>();
+		
+		for (String registro : registros) {
+			
+			hashMapPalabras.put(registro, new HashMap<Character, Integer>());
+			
+			for (int i = 0; i< registro.length(); i++) {
+				HashMap<Character, Integer> hashMapinterno = hashMapPalabras.get(registro);
+				
+				char caracter = registro.charAt(i);
+				
+				if (hashMapinterno.isEmpty() || hashMapinterno.get(caracter) == null) {
+					hashMapinterno.put(caracter, 1);
+				}
+				
+				else {
+					int cantidad = hashMapinterno.get(caracter) + 1;
+					
+					hashMapinterno.put(caracter, cantidad);
+				}
+			}
+			
+		}
+		
+        for (String innerKey : hashMapPalabras.keySet()) {
+            System.out.println("  Palabra: " + innerKey + ", Letras: " + hashMapPalabras.get(innerKey));
+        }
+		
 	}
 
 }
